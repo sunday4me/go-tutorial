abae2ec56bde0fcd707ff07920383fc159f65606
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-01 23:55:57 +0100

    Update array.go

--- array.go
+++ array.go
@@ -4,7 +4,23 @@ import "fmt"
 
 func main() {
 arr  := [3] int{4, 5, 6 }
+  
+   //fmt.Println(len(arr))
 
-fmt.Println(arr)
+  // to loop through
+
+sum := 0 
+  
+  for  i := 0; i < len(arr); i++1 {
+sum += arr[i]
+    
+  }  
+  fmt.Println(sum)
+  
+  // multi array that is array inside array
+    arr2D := [2][3]int {{1, 2, 3},{3, 3, 4}}
+fmt.Println(arr2D)
+  
+ 
 
 }
