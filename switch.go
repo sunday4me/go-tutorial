dca5706c91851689ba3daf04d9205ffbc06b69ea
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-01-01 23:25:56 +0100

    switch in golang

---
+++ switch.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+ans := 10
+
+switch ans {
+case 1:
+fmt.Println("1. One")
+fmt.Println("2. One")
+
+case 2:
+fmt.Println("two")
+
+default:
+fmt.Println("not a case")
